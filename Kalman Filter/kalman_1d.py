e13fc384dd26faeca75bd4be29b2640020d2c918
Daniel Ingram <ingramds@appstate.edu>
2018-10-30 14:05:28 -0400

    1d Kalman

--- Kalman Filter/kalman_1d.py
+++ Kalman Filter/kalman_1d.py
@@ -16,13 +16,13 @@ def predict(mean1, var1, mean2, var2):
     new_var = var1 + var2
     return new_mean, new_var
 
-measurement_var = 0.4
+measurement_var = 0.5
 motion_var = 0.5
-measurements = [gauss(i, measurement_var) for i in range(100)]
-motion = [gauss(1, motion_var) for i in range(100)]
+measurements = [gauss(i, measurement_var) for i in range(50)]
+motion = [gauss(1, motion_var) for i in range(50)]
 mean = 0
 var = 10000
-x_pts = [i/10 for i in range(1000)]
+x_pts = [i/10 for i in range(500)]
 
 plt.ion()
 fig, ax = plt.subplots()
@@ -31,11 +31,13 @@ for meas, move in zip(measurements, motion):
     mean, var = predict(mean, var, move, motion_var)
     y1 = [f(mean, var, x) for x in x_pts]
     mean, var = update(mean, var, meas, measurement_var)
-    y2 = [f(mean, var, x) for x in x_pts]
+    y2 = [f(meas, measurement_var, x) for x in x_pts]
+    y3 = [f(mean, var, x) for x in x_pts]
     ax.cla()
     ax.plot(x_pts, y1, 'r-')
     ax.plot(x_pts, y2, 'g-')
-    ax.set_xlim(0, 100)
+    ax.plot(x_pts, y3, 'b-')
+    ax.set_xlim(0, 50)
     ax.set_ylim(0, 1)
-    ax.legend(("Prediction", "Update"))
-    plt.pause(1e-3)
+    ax.legend(("Prediction", "Measurement", "Update"))
+    plt.pause(1e-1)
