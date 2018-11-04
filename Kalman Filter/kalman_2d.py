cbb2b94e90f05db73fb8046b272a58019814168d
Daniel Ingram <ingramds@appstate.edu>
2018-11-03 23:00:27 -0400

    Add random motion

--- Kalman Filter/kalman_2d.py
+++ Kalman Filter/kalman_2d.py
@@ -1,8 +1,8 @@
 from __future__ import print_function, division
+import random
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-from mpl_toolkits.mplot3d import Axes3D
 
 dt = 0.1
 I = np.identity(4)                  #Identity matrix
@@ -12,14 +12,14 @@ F = np.array([[1, 0, dt, 0],        #State transition matrix
               [0, 0, 0, 1]])
 H = np.array([[1, 0, 0, 0],         #Measurement function
               [0, 1, 0, 0]])
-R = np.array([[100, 0],             #Measurement noise
+R = np.array([[100, 0],            #Measurement noise
               [0, 100]])
-x = np.array([[0],                  #State estimate
-              [0],
+x = np.array([[50],                 #State estimate
+              [50],
               [0],
               [0]])
-P = np.array([[1, 0, 0, 0],         #Uncertainty covariance
-              [0, 1, 0, 0],
+P = np.array([[1000, 0, 0, 0],      #Uncertainty covariance
+              [0, 1000, 0, 0],
               [0, 0, 1000, 0],
               [0, 0, 0, 1000]])
 u = np.array([[0],                  #Control
@@ -46,27 +46,42 @@ def gaussian_2d(X, Y, x, P):
 #plt.ion()
 fig, ax = plt.subplots()
 X, Y = np.meshgrid([1.0*i for i in range(100)], [1.0*i for i in range(100)])
-measurements = np.array([[[i, i]] for i in range(100)])
+possible_motions = [[1, 0, 0, 0],
+                    [0, 1, 0, 0],
+                    [-1, 0, 0, 0],
+                    [0, -1, 0, 0],
+                    [1, 1, 0, 0],
+                    [1, -1, 0, 0],
+                    [-1, 1, 0, 0],
+                    [-1, -1, 0, 0]]
+motions = np.array([random.choice(possible_motions) for _ in range(500)])
+measurements = np.array([[[50, 50]] for _ in range(500)])
+for i in range(1, 500):
+    measurements[i, 0, 0] = measurements[i-1, 0, 0] + motions[i, 0]
+    measurements[i, 0, 1] = measurements[i-1, 0, 1] + motions[i, 1]
+
 f = gaussian_2d(X, Y, x, P)
 grid = ax.imshow(f)
 dot, = ax.plot(0, 0, 'r.')
 def animate(i):
     global x, P
-    x, P = predict(x, u, P)
-    x, P = update(x, measurements[i], P)
+    x, P = predict(x, motions[i].reshape(4, 1), P)
+    x, P = update(x, measurements[i, 0].reshape(1, 2), P)
     f = gaussian_2d(X, Y, x, P)
     grid.set_data(f)
     smallest = min(min(row) for row in f)
     largest = max(max(row) for row in f)
     grid.set_clim(vmin=0, vmax=largest)
-    dot.set_data([i, i])
+    dot.set_data(measurements[i, 0, :2])
     return grid, dot,
 
 def init():
     ax.set_xlim(0, 100)
     ax.set_ylim(0, 100)
+    ax.set_xlabel(r"x")
+    ax.set_ylabel(r"y")
     return grid, dot,
 
-anim = animation.FuncAnimation(fig, animate, 100, interval=50, init_func=init)
+anim = animation.FuncAnimation(fig, animate, 500, interval=50, init_func=init)
 #plt.show()
 anim.save("kalman_2d.gif", writer="imagemagick")
